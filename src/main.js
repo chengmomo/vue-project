c54322e02439d2c12640cc5d681902bf1f8bd3cd
zhaichengjuan <z923891099@icloud.com>
2018-11-05 15:18:50 +0800

    引入阿里图标库

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
-
-//引入element-ui
+import './assets/icon/iconfont.css' // 阿里图标
+// 引入echarts
+import echarts from 'echarts'
+// 引入element-ui
 import ElementUI from 'element-ui'
-//引入element-ui的默认CSS样式
+// 引入element-ui的默认CSS样式
 import 'element-ui/lib/theme-chalk/index.css'
 // import '../static/css/theme-green/index.css';   // 浅绿色主题
 Vue.use(ElementUI)
+Vue.prototype.$echarts = echarts
 
 // // 引入vue-quill-editor
 // import VueQuillEditor from 'vue-quill-editor'
@@ -18,14 +21,10 @@ Vue.use(ElementUI)
 // import 'quill/dist/quill.core.css'
 // import 'quill/dist/quill.snow.css'
 // import 'quill/dist/quill.bubble.css'
-// 引入echarts
-import echarts from 'echarts'
 // import mavonEditor from 'mavon-editor'
 // import 'mavon-editor/dist/css/index.css'
-
 // Vue.use(VueQuillEditor)
 // Vue.use(mavonEditor)
-Vue.prototype.$echarts = echarts 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
